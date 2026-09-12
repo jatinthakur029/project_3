38ca5cec03abe145fc4b4235bc08d7b55d553cf8
jatinthakur029 <jatinthakur432156@gmail.com>
2026-09-12 22:11:16 +0530

    doc fix

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -64,7 +64,7 @@ resource "aws_eks_node_group" "example" {
     max_unavailable = 1
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
   aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
